c0fbe79b6fb9bc3b0c1c8596cab2e297cff35dc9
berrima <berrima_tahar@yahoo.com>
2015-02-25 21:04:38 +0100

    Changed license

--- examples/SpeedGauge/mainwindow.h
+++ examples/SpeedGauge/mainwindow.h
@@ -2,20 +2,21 @@
 **                                                                        **
 **  QcGauge, for instrumentation, and real time data measurement          **
 **  visualization widget for Qt.                                          **
-**  Copyright (C) 2014 Hadj Tahar Berrima                                 **
+**  Copyright (C) 2015 Hadj Tahar Berrima                                 **
 **                                                                        **
 **  This program is free software: you can redistribute it and/or modify  **
-**  it under the terms of the GNU General Public License as published by  **
-**  the Free Software Foundation, either version 3 of the License, or     **
-**  (at your option) any later version.                                   **
+**  it under the terms of the GNU Lesser General Public License as        **
+**  published by the Free Software Foundation, either version 3 of the    **
+**  License, or (at your option) any later version.                       **
 **                                                                        **
 **  This program is distributed in the hope that it will be useful,       **
 **  but WITHOUT ANY WARRANTY; without even the implied warranty of        **
 **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         **
-**  GNU General Public License for more details.                          **
+**  GNU Lesser General Public License for more details.                   **
 **                                                                        **
-**  You should have received a copy of the GNU General Public License     **
-**  along with this program.  If not, see http://www.gnu.org/licenses/.   **
+**  You should have received a copy of the GNU Lesser General Public      **
+**  License along with this program.                                      **
+**  If not, see http://www.gnu.org/licenses/.                             **
 **                                                                        **
 ****************************************************************************
 **           Author:  Hadj Tahar Berrima                                  **
